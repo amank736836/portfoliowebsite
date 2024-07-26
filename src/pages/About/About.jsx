f5f1b084bab031b624c21be1bc894d11a10b8cfd
Aman Kumar <amankarguwal0@gmail.com>
2024-07-26 17:45:57 +0530

    feat: Update About page with personal info, resume download, stats, and skills
    The code changes in the About page component add personal information, a resume download button, statistics, and skills section. This feature enhances the user experience by providing detailed information about the user, allowing them to download the resume, and showcasing their skills and achievements.

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import Info from "../../Components/Info";
+import { FaDownload } from "react-icons/fa";
+import Resume from "../../assets/Aman.pdf";
+import Stats from "../../Components/Stats";
+import Skills from "../../Components/Skills";
+import "./About.css";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <main className="section container">
+      <section className="about">
+        <h2>
+          <div className="section__title">
+            About <span>Me</span>
+          </div>
+        </h2>
+        <div className="about__container grid">
+          <div className="about__info">
+            <h3 className="section__subtitle">Personal Infos</h3>
+            <ul className="info__list grid">
+              <Info />
+            </ul>
+            <a href={Resume}
+              download=''
+            className="button">
+              Download CV{" "}
+              <span className="button__icon">
+                <FaDownload />
+              </span>
+            </a>
+          </div>
+          <div className="stats grid">
+            <Stats/>
+          </div>
+        </div>
+      </section>
 
-export default About
+      <div className="separator"></div>
+
+      <section className="skills">
+        <h3 className="section__subtitle subtitle__center">My Skills</h3>
+        <div className="skills__container grid">
+          <Skills />
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default About;
