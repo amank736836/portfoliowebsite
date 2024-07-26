6d4c2361fba4f5b34f1c6b0bac71ac77c0b1e40d
Aman Kumar <amankarguwal0@gmail.com>
2024-07-26 16:23:55 +0530

    feat: Update Home page with profile image and content
    The code changes update the Home page component to include a profile image, a title, a description, and a button for more information. This feature enhances the user experience by providing a visually appealing and informative landing page for the application.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
-
+import Profile from "../../assets/home2.jpg";
+import { Link } from "react-router-dom";
+import { FaArrowRight } from "react-icons/fa";
+import "./Home.css";
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className="home section grid">
+      <img src={Profile} alt="" className="home__img" />
+      <div className="home__content">
+        <div className="home__data">
+          <h1 className="home__title">
+            <span>I&apos;m Aman Kumar. </span>
+            Full Stack Developer
+          </h1>
+          <p className="home__description">
+            Passionate Full Stack Developer with Expertise in MERN Stack <br />
+            Crafting Dynamic Web Applications with React, Node.js, and Tailwind
+            CSS <br /> Top Performer in Hackathons and Skilled in Solving
+            Complex Algorithms <br /> Dedicated to Building Innovative Solutions
+            and Enhancing User Experiences
+          </p>
+          <Link to="/about" className="button">
+            More About Me
+            <span className="button__icon">
+              <FaArrowRight />
+            </span>
+          </Link>
+        </div>
+      </div>
 
-export default Home
+      <div className="color__block"></div>
+    </section>
+  );
+};
+
+export default Home;
