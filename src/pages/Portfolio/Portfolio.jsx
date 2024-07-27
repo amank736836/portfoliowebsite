9b21bbdf7a7e3011b290f04655b86894c753bc77
Aman Kumar <amankarguwal0@gmail.com>
2024-07-27 21:48:14 +0530

    feat: Update Portfolio page with PortfolioItem component

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import PortfolioItem from "../../Components/PortfolioItem";
+import "./Portfolio.css";
+import { portfolio } from "../../data";
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
-  )
-}
+    <section className="portfolio section">
+      <h2 className="section__title">
+        My <span>Portfolio</span>
+      </h2>
+      <div className="portfolio__container container grid">
+        {portfolio.map((item) => {
+          return <PortfolioItem key={item.id} {...item}/>;
+        })}
+      </div>
+    </section>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
