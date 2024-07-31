84dfef60b4247eeb870ec8374f3c828f95292a7f
Aman Kumar <amankarguwal0@gmail.com>
2024-07-31 14:08:46 +0530

    ```text refactor: Remove CircularProgressbar from Skills component
    The code changes in Skills.jsx remove the import statement for CircularProgressbar and the corresponding JSX code. This refactor simplifies the Skills component by removing the circular progress bar feature, which was not being used.

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,21 +1,13 @@
-import { skills } from '../data'
-import { CircularProgressbar } from 'react-circular-progressbar'
 import 'react-circular-progressbar/dist/styles.css'
+import { skills } from '../data'
 
 const Skills = () => {
   return (
     <>
         {
-            skills.map(({ title, percentage }, index) => {
+            skills.map(({ title }, index) => {
                 return (
                     <div className="progress__box" key={index}>
-                        <div className="progress__circle">
-                            <CircularProgressbar
-                                strokeWidth={7.5}
-                                text={`${percentage}%`}
-                                value={percentage}
-                            />
-                        </div>
                         <h3 className="skills__title">{title}</h3>
                     </div>
                 )
