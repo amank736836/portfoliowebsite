6cd658a02ac46e314ede9740333188ee3fc8c9b2
Aman Kumar <amankarguwal0@gmail.com>
2024-08-11 13:53:51 +0530

    feat: Add Resume page component
    This commit adds a new component, Resume.jsx, which represents the Resume page of the application. The component renders an iframe that displays a Google Docs document embedded in the page. This addition enhances the functionality of the application by providing a dedicated page for viewing the user's resume.

---
+++ src/pages/Resume/Resume.jsx
@@ -0,0 +1,26 @@
+const Resume = () => {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        top: 0,
+        left: 0,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#f1f1f1",
+      }}
+    >
+      <iframe
+        src="https://docs.google.com/document/d/e/2PACX-1vTuWokH7Wlj5zIUhk8WRmzf9Hc4_HyBK3TvJW9tRHhTzpFD27uESgjDusk9-Imj28Fq6r5fqtFbTni6/pub?embedded=true"
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+      ></iframe>
+    </div>
+  );
+};
+
+export default Resume;
