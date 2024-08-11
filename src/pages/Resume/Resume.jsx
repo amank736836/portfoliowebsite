37a4792b4c1250e139fc58d97d87df49e38c8100
Aman Kumar <amankarguwal0@gmail.com>
2024-08-11 14:39:04 +0530

    feat: Improve Resume page load performance with lazy loading for images

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -2,24 +2,32 @@ const Resume = () => {
   return (
     <div
       style={{
-        width: "100%",
-        height: "100%",
-        position: "absolute",
-        top: 0,
-        left: 0,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#f1f1f1",
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "var(--body-color)",
       }}
     >
-      <iframe
-        src="https://docs.google.com/document/d/e/2PACX-1vTuWokH7Wlj5zIUhk8WRmzf9Hc4_HyBK3TvJW9tRHhTzpFD27uESgjDusk9-Imj28Fq6r5fqtFbTni6/pub?embedded=true"
+      <div
+        className="resumeFile"
         style={{
-          width: "100%",
-          height: "100%",
+          width: "100vw",
+          height: "100vh",
+          position: "absolute",
+          top: "0",
+          left: "0",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
         }}
-      ></iframe>
+      >
+        <iframe
+          src="https://docs.google.com/document/d/e/2PACX-1vTuWokH7Wlj5zIUhk8WRmzf9Hc4_HyBK3TvJW9tRHhTzpFD27uESgjDusk9-Imj28Fq6r5fqtFbTni6/pub?embedded=true"
+          style={{
+            width: "100vw",
+            height: "100vh",
+          }}
+        ></iframe>
+      </div>
     </div>
   );
 };
